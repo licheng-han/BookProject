6cd68f03cb5f01664ec5b74842bbff024ca462d9
lichen <543496598@qq.com>
2020-06-22 01:53:48 +0800

    练手的header

--- src/header/index.js
+++ src/header/index.js
@@ -1,8 +1,63 @@
-import React from "react";
-import {login} from '../css/header.js'
-function Header(props) {
-    return (
-        <login />
-    );
+import React, {Component} from "react";
+import {
+    HeaderWrapper,
+    Logo,
+    Nav,
+    NavItem,
+    NavSearch,
+    Addition,
+    Button
+} from '../css/header.js'
+import { CSSTransition } from 'react-transition-group';
+
+
+class Header extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            focused:false
+        };
+        this.onblur = this.onblur.bind(this);
+        this.onfocus = this.onfocus.bind(this);
+    }
+    onblur(){
+        this.setState({
+            focused:false
+        })
+    }
+    onfocus(){
+        this.setState((preloadState)=>{
+            return {
+                focused:true
+            }
+        })
+    }
+    render() {
+        return (
+            <HeaderWrapper>
+                <Logo/>
+                <Nav>
+                    <NavItem className='left active'>首页</NavItem>
+                    <NavItem className="left">下载App</NavItem>
+                    <NavItem className="right">登陆</NavItem>
+                    <NavItem className="right">
+                        <i className="iconfont">&#xe636;</i>
+                    </NavItem>
+                    <CSSTransition
+                        in={this.state.focused}
+                        timeout={200}
+                        classNames="slide"
+                    >
+                        <NavSearch  onBlur={this.onblur} onFocus={this.onfocus}/>
+                    </CSSTransition>
+                </Nav>
+                <Addition>
+                    <Button className="reg">注册</Button>
+                    <Button>写文章</Button>
+                </Addition>
+            </HeaderWrapper>
+        )
+    }
 }
+
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React,{Fragment}from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {GlobalStyle} from './style.js';
+import {FontStyle} from './statics/iconfont/iconfont';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+    <Fragment>
+      <App />
+        <GlobalStyle />
+        <FontStyle />
+    </Fragment>,
+  // </React.StrictMode>,
   document.getElementById('root')
 );
 // If you want your app to work offline and load faster, you can change
