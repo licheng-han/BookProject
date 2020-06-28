ae5faa97be52d56885ec28df423053ebd8b8d4a8
lichen <543496598@qq.com>
2020-06-28 19:54:14 +0800

    添加header已经环境

---
+++ src/header/store/index.js
@@ -0,0 +1,8 @@
+import * as actionCreater from './actionCreater';
+import * as constants from './constants';
+import  reducer from './reducer';
+export {
+    actionCreater,
+    constants,
+    reducer
+}
